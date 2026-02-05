1cd1bde5cb6cf3f8a5a53ece0973073dd6c6f876
Daniel Segura <danielseguraf@gmail.com>
2026-02-04 20:59:12 -0500

    remove validation schema

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -8,14 +8,12 @@ import { SeedModule } from './seed/seed.module'
 import { CommonAreasModule } from './common-areas/common-areas.module'
 import { ReservationsModule } from './reservations/reservations.module'
 import { envConfig } from 'src/config/env.config'
-import { envSchema } from 'src/config/env.validation'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [envConfig],
-      validationSchema: envSchema,
     }),
     PrinterModule,
     PrismaModule,
